e6e087af6b43738bd3af0fe93ea62ce4851384b1
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-19 19:46:32 +0430

    building the Sidebar and SidebarRow components

---
+++ components/Siderbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+BellIcon,
+HashtagIcon,
+BookmarkIcon,
+CollectionIcon,
+DotsCircleHorizontalIcon,
+MailIcon,
+UserIcon,
+HomeIcon
+} from '@heroicons/react/outline'
+import SiderBarRow from './SiderBarRow'
+
+function Siderbar() {
+  return (
+    <div className='flex flex-col'>
+        <img className='w-10 h-10' alt='logo' src='https://links.papareact.com/drq' />
+        <SiderBarRow Icon={HomeIcon} title="Home" />
+        <SiderBarRow Icon={HashtagIcon} title="Explore" />
+        <SiderBarRow Icon={BellIcon} title="Notifiction" />
+        <SiderBarRow Icon={MailIcon} title="Messages" />
+        <SiderBarRow Icon={BookmarkIcon} title="Bookmarks" />
+        <SiderBarRow Icon={CollectionIcon} title="Lists" />
+        <SiderBarRow Icon={UserIcon} title="Sign In" />
+        <SiderBarRow Icon={DotsCircleHorizontalIcon} title="More" />
+    </div>
+  )
+}
+
+export default Siderbar
