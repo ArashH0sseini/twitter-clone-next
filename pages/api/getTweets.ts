21f8dc441c45eab8f69acb38f89e96c670f73a37
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-20 12:02:25 +0430

    creating a type definition file using typeScript

--- pages/api/getTweets.ts
+++ pages/api/getTweets.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { sanityClient } from "../../sanity";
+import { Tweet } from "../../typings";
 
 type Data = {
   tweets: Tweet[];
