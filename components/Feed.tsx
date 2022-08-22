91606c0d59825cec1ed1f2fcf6df33f7d0f0d070
Arash Hosseini <arashhosseini733@gmail.com>
2022-08-22 11:44:50 +0430

    adding tweet functionality using mutations

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -29,13 +29,11 @@ function Feed({ tweets: tweetsProp }: Props) {
         <h1 className="p-5 pb-0 text-xl font-bold">Home</h1>
         <RefreshIcon
           onClick={handleRefresh}
-          className="mt-5 mr-5 h-8 w-8 cursor-pointer 
-        text-twitter transition-all 
-        duration-500 ease-out hover:rotate-180 active:scale-125"
+          className="w-8 h-8 mt-5 mr-5 transition-all duration-500 ease-out cursor-pointer text-twitter hover:rotate-180 active:scale-125"
         />
       </div>
       <div>
-        <TweetBox />
+        <TweetBox setTweets={setTweets} />
       </div>
       <div>
         {tweets.map((tweet) => (
--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -6,9 +6,22 @@ import {
   SearchCircleIcon,
 } from "@heroicons/react/outline";
 import { useSession } from "next-auth/react";
-import React, { useRef, useState } from "react";
+import React, {
+  Dispatch,
+  MouseEvent,
+  SetStateAction,
+  useRef,
+  useState,
+} from "react";
+import toast from "react-hot-toast";
+import { Tweet, TweetBody } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 
-function TweetBox() {
+interface Props {
+  setTweets: Dispatch<SetStateAction<Tweet[]>>;
+}
+
+function TweetBox({ setTweets }: Props) {
   const [input, setInput] = useState<string>("");
   const [image, setImage] = useState<string>("");
 
@@ -18,7 +31,7 @@ function TweetBox() {
   const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false);
 
   const addImageToTweet = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+    e: React.MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
   ) => {
     e.preventDefault();
 
@@ -28,56 +41,86 @@ function TweetBox() {
     imageInputRef.current.value = "";
     setImageUrlBoxIsOpen(false);
   };
+
+  const postTweet = async () => {
+    const tweetInfo: TweetBody = {
+      text: input,
+      username: session?.user?.name || "Unknown User",
+      profileImg: session?.user?.image || "https://links.papareact.com/gll",
+      image: image,
+    };
+
+    const result = await fetch(`/api/addTweet`, {
+      body: JSON.stringify(tweetInfo),
+      method: "POST",
+    });
+
+    const json = await result.json();
+
+    const newTweets = await fetchTweets();
+    setTweets(newTweets);
+
+    toast("Tweet Posted", {
+      icon: "🚀",
+    });
+
+    return json;
+  };
+
+  const handleSubmit = (
+    e: MouseEvent<HTMLButtonElement, globalThis.MouseEvent>
+  ) => {
+    e.preventDefault();
+
+    postTweet();
+
+    setInput("");
+    setImage("");
+    setImageUrlBoxIsOpen(false);
+  };
+
   return (
-    <div className="flex space-x-2 p-5">
+    <div className="flex p-5 space-x-2">
       <img
-        className="h-14 w-14 mt-4 rounded-full 
-        object-cover"
+        className="object-cover mt-4 rounded-full h-14 w-14"
         src={session?.user?.image || "https://links.papareact.com/gll"}
         alt=""
       />
 
-      <div className="flex flex-1 items-center pl-2">
-        <form className="flex flex-1 flex-col">
+      <div className="flex items-center flex-1 pl-2">
+        <form className="flex flex-col flex-1">
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
             type="text"
             placeholder="what's Happening"
-            className="h-24 w-full text-xl outline-none 
-                placeholder:text-xl"
+            className="w-full h-24 text-xl outline-none placeholder:text-xl"
           />
           <div className="flex items-center">
-            <div className="flex space-x-2 text-twitter flex-1">
+            <div className="flex flex-1 space-x-2 text-twitter">
               <PhotographIcon
                 onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
-                className="h-5 w-5
-                        cursor-pointer transition-transform duration-150 ease-out
-                        hover:scale-150"
+                className="w-5 h-5 transition-transform duration-150 ease-out cursor-pointer hover:scale-150"
               />
-              <SearchCircleIcon className="h-5 w-5" />
-              <EmojiHappyIcon className="h-5 w-5" />
-              <CalendarIcon className="h-5 w-5" />
-              <LocationMarkerIcon className="h-5 w-5" />
+              <SearchCircleIcon className="w-5 h-5" />
+              <EmojiHappyIcon className="w-5 h-5" />
+              <CalendarIcon className="w-5 h-5" />
+              <LocationMarkerIcon className="w-5 h-5" />
             </div>
 
             <button
+              onClick={handleSubmit}
               disabled={!input || !session}
-              className="bg-twitter px-5 py-2 text-white
-                    rounded-full disabled:opacity-40"
+              className="px-5 py-2 text-white rounded-full bg-twitter disabled:opacity-40"
             >
               Tweet
             </button>
           </div>
           {imageUrlBoxIsOpen && (
-            <form
-              className="rounded-lg mt-5 flex bg-twitter/80
-            py-2 px-4"
-            >
+            <form className="flex px-4 py-2 mt-5 rounded-lg bg-twitter/80">
               <input
                 ref={imageInputRef}
-                className="flex-1 bg-transparent p-2
-              text-white outline-none placeholder:text-white"
+                className="flex-1 p-2 text-white bg-transparent outline-none placeholder:text-white"
                 type="text"
                 placeholder="Enter Image Url ..."
               />
@@ -89,6 +132,13 @@ function TweetBox() {
                 Add Image
               </button>
             </form>
+          )}
+          {image && (
+            <img
+              className="object-contain w-full h-40 mt-10 shadow-lg rounded-xl"
+              src={image}
+              alt=""
+            />
           )}
         </form>
       </div>
